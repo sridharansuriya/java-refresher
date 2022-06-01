4df8a0a6086bb0f1fbadb485e0e44d0499424b0a
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-01 11:12:06 +0530

    Autoboxing

--- src/WrapperDemo.java
+++ src/WrapperDemo.java
@@ -1,4 +1,10 @@
 public class WrapperDemo {
 	public static void main(String[] args) {
+		int i = 5;
+		Integer ii = new Integer(i); // Boxing/Wrapping
+		int j = ii.intValue(); // Unboxing/unwrapping
+		
+		Integer k = i; // Autoboxing
+		int l = k; // Autounboxing/Autounwrapping
 	}
 }
