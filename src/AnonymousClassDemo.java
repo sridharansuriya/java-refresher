2ce70a92719eff68b670812eaff306ced39ffb71
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-02 21:48:03 +0530

    Anonymous classes

---
+++ src/AnonymousClassDemo.java
@@ -0,0 +1,16 @@
+interface Abc {
+	void show();
+}
+
+public class AnonymousClassDemo {
+	public static void main(String[] args) {
+		Abc obj = new Abc() {
+			@Override
+			public void show() {
+				System.out.println("in show");
+			}		
+		};
+		
+		obj.show();
+	}
+}
