fd05ed64a5cac43aef8feb5fb243a3f813a076bd
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-02 21:36:01 +0530

    interface

---
+++ src/InterfaceDemo.java
@@ -0,0 +1,33 @@
+interface WriterInterface {
+	void write();
+}
+
+class Sketch implements WriterInterface {
+	@Override
+	public void write() {
+		System.out.println("I am a sketch pen");
+	}
+}
+
+class Crayon implements WriterInterface {
+	@Override
+	public void write() {
+		System.out.println("I am a crayon");
+	}
+}
+
+class Kit {
+	public void doSomething(WriterInterface w) {
+		w.write();
+	}
+}
+
+public class InterfaceDemo {
+	public static void main(String[] args) {
+		Kit k = new Kit();
+		WriterInterface s = new Sketch();
+		k.doSomething(s);
+		WriterInterface c = new Crayon();
+		k.doSomething(c);
+	}
+}
