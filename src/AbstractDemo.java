b92a456f6cb0c70aee1895b40c80fc8022b33ead
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-02 20:22:25 +0530

    Abstract classes

---
+++ src/AbstractDemo.java
@@ -0,0 +1,22 @@
+abstract class Writer {
+	public void write(String input) {
+		System.out.println("Writing text: " + input);
+	}
+}
+
+class Pen extends Writer {
+	
+}
+
+class Pencil extends Writer {
+	
+}
+
+public class AbstractDemo {
+	public static void main(String[] args) {
+		Writer w = new Pen();
+		w.write("Pen text");
+		w = new Pencil();
+		w.write("Pencil text");
+	}
+}
