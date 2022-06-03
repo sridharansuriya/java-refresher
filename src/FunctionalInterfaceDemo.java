0a1f4bb889049fb5c7e169cec37ad1a34d7b49eb
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-03 08:22:09 +0530

    functional interface

---
+++ src/FunctionalInterfaceDemo.java
@@ -0,0 +1,11 @@
+@FunctionalInterface  //This is not needed. But with this the compiler will check the interface has one and only one method
+interface Functional {
+	void show();
+}
+
+public class FunctionalInterfaceDemo {
+	public static void main(String[] args) {
+		Functional obj = () -> System.out.println("Hi there"); // this is the definition for show method
+		obj.show();
+	}
+}
