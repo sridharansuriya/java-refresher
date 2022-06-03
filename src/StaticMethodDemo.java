95170d2f5042321fae3a347dc0f4d04c176975be
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-03 08:47:01 +0530

    static method

---
+++ src/StaticMethodDemo.java
@@ -0,0 +1,11 @@
+interface Static {
+	static void abc() {
+		System.out.println("In abc");
+	}
+}
+
+public class StaticMethodDemo {
+	public static void main(String[] args) {
+		Static.abc();
+	}
+}
