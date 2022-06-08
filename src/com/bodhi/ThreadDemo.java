ecd204fb046f0761c40db8ba045ae11f0ae16160
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-08 22:19:05 +0530

    Lambda expression and isAlive and join

--- src/com/bodhi/ThreadDemo.java
+++ src/com/bodhi/ThreadDemo.java
@@ -1,46 +1,27 @@
 package com.bodhi;
 
-class Hi implements Runnable {
-	@Override
-	public void run() {
-		for (int i = 0; i < 5; ++i) {
-			System.out.println("Hi");
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
-
-class Hello implements Runnable {
-	@Override
-	public void run() {
-		for (int i = 0; i < 5; ++i) {
-			System.out.println("Hello");
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-}
-
 public class ThreadDemo {
-	public static void main(String[] args) {
-		Runnable hi = new Hi();
-		Runnable hello = new Hello();
-		Thread t1 = new Thread(hi);
-		Thread t2 = new Thread(hello);
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(() -> {
+			for (int i = 0; i < 5; ++i) {
+				System.out.println("Hi");
+				try {	Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+			}
+		});
+		Thread t2 = new Thread(() -> {
+			for (int i = 0; i < 5; ++i) {
+				System.out.println("Hello");
+				try {	Thread.sleep(500);} catch (InterruptedException e) {e.printStackTrace();}
+			}
+		});
 		t1.start();
 		// introducing a minor delay to maintain sequence.
-		try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		try {	Thread.sleep(10);} catch (InterruptedException e) {e.printStackTrace();}
 		t2.start();
+
+		t1.join();
+		t2.join();
+
+		System.out.println("Bye");
 	}
 }
