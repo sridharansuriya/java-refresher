064e49f2d81f2bfaebbffd3543abf9ba3313f8c0
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-09 13:17:48 +0530

    Generics

--- src/com/bodhi/CollectionDemo.java
+++ src/com/bodhi/CollectionDemo.java
@@ -1,18 +1,17 @@
 package com.bodhi;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class CollectionDemo {
 	public static void main(String[] args) {
-		List values = new ArrayList();
+		List<Integer> values = new ArrayList<>();  // Usage of generics
 		values.add(4);
 		values.add(6);
 		values.add(9);
-		values.add(2, "2");
+		values.add(2, 2);
 		
-		for(Object o: values) {
+		for(Integer o: values) {
 			System.out.println(o);
 		}
 	}
