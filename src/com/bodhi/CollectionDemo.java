c138f5606badb07202ac4332ec94751019ddc954
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-09 13:13:08 +0530

    List Interface

--- src/com/bodhi/CollectionDemo.java
+++ src/com/bodhi/CollectionDemo.java
@@ -1,18 +1,19 @@
 package com.bodhi;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class CollectionDemo {
 	public static void main(String[] args) {
-		Collection values = new ArrayList();
+		List values = new ArrayList();
 		values.add(4);
 		values.add(6);
 		values.add(9);
+		values.add(2, "2");
 		
-		Iterator it = values.iterator();
-		while(it.hasNext())
-			System.out.println(it.next());
+		for(Object o: values) {
+			System.out.println(o);
+		}
 	}
 }
