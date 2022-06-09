4b91c7dae7fb55fc927f000cb6b9959aaaaa4cf6
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-09 14:04:41 +0530

    Comparator and Comparable

--- src/com/bodhi/CollectionDemo.java
+++ src/com/bodhi/CollectionDemo.java
@@ -4,6 +4,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+class Stud implements Comparable<Stud> {
+	private int rollno;
+	private String name;
+	private int marks;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Stud(int rollno, String name, int marks) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Stud [rollno=" + rollno + ", name=" + name + ", marks=" + marks + "]";
+	}
+
+	@Override
+	public int compareTo(Stud o) {
+		// TODO Auto-generated method stub
+		return marks > o.marks ? 1 : -1;
+	}
+}
+
 public class CollectionDemo {
 	public static void main(String[] args) {
 		List<Integer> values = new ArrayList<>(); // Usage of generics
@@ -17,7 +49,7 @@ public class CollectionDemo {
 		for (Integer o : values) {
 			System.out.println(o);
 		}
-		
+
 		System.out.println();
 		System.out.println();
 
@@ -25,10 +57,31 @@ public class CollectionDemo {
 		// This sorts based on last digit
 		// Not we are using lambda expression here to implement comparator interface
 		Collections.sort(values, (i, j) -> i % 10 > j % 10 ? 1 : -1);
-		
+
 		for (Integer o : values) {
 			System.out.println(o);
 		}
-		
+
+		List<Stud> studs = new ArrayList<>();
+		studs.add(new Stud(23, "Suriya", 70));
+		studs.add(new Stud(24, "Srinidhi", 80));
+		studs.add(new Stud(25, "Madhavan", 60));
+
+		Collections.sort(studs);
+
+		System.out.println();
+		System.out.println();
+		for (Stud s : studs) {
+			System.out.println(s);
+		}
+		System.out.println();
+		System.out.println();
+
+		// Sorting using comparator which takes precedence over comparable.
+		Collections.sort(studs, (i, j) -> i.getName().length() > j.getName().length() ? 1 : -1);
+		for (Stud s : studs) {
+			System.out.println(s);
+		}
+
 	}
 }
