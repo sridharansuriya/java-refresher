ddf06a12f63ff34223c1ed3e7bfbcae26e6ddc4a
Suriyanarayanan.s <asterixsurya@gmail.com>
2022-06-09 13:48:14 +0530

    Comparator Interface

--- src/com/bodhi/CollectionDemo.java
+++ src/com/bodhi/CollectionDemo.java
@@ -1,18 +1,34 @@
 package com.bodhi;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CollectionDemo {
 	public static void main(String[] args) {
-		List<Integer> values = new ArrayList<>();  // Usage of generics
-		values.add(4);
-		values.add(6);
-		values.add(9);
-		values.add(2, 2);
-		
-		for(Integer o: values) {
+		List<Integer> values = new ArrayList<>(); // Usage of generics
+		values.add(404);
+		values.add(639);
+		values.add(908);
+		values.add(265);
+
+		Collections.sort(values);
+
+		for (Integer o : values) {
 			System.out.println(o);
 		}
+		
+		System.out.println();
+		System.out.println();
+
+		// Using comparator interface
+		// This sorts based on last digit
+		// Not we are using lambda expression here to implement comparator interface
+		Collections.sort(values, (i, j) -> i % 10 > j % 10 ? 1 : -1);
+		
+		for (Integer o : values) {
+			System.out.println(o);
+		}
+		
 	}
 }
